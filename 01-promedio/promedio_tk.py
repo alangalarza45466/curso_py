3bc1da134d7bdc32d37ca8e7a30f218a64725f09
alan galarza <alangalarza280@gmail.com>
2025-05-17 15:09:06 +0000

    add position

--- 01-promedio/promedio_tk.py
+++ 01-promedio/promedio_tk.py
@@ -4,7 +4,7 @@ import tkinter as tk
 # crear ventana
 root = tk.Tk()
 # configuracion root
-root.title("...")
+root.title("promedio")
 root.minsize(350,150)
 root.resizable(0,0)
 
@@ -16,11 +16,15 @@ label_nota3 = tk.Label(root,text="Ingrese la tercera nota: ")
 
 # caja de txt
 text_nota1 = tk.StringVar()
+text_nota2 = tk.StringVar()
+text_nota3 = tk.StringVar()
 
 # entrada de txt
 entry_nota1 = tk.Entry(root,width=20,textvariable=text_nota1)
 # posicion de los elementos
 # label
+label_nota3.grid(row=2, column=0)
+label_nota2.grid(row=1, column=0)
 label_nota1.grid(row=0, column=0)
 # entrada de txt
 entry_nota1.grid(row=0, column=1)
