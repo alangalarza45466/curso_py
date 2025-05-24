502ec00f66dca907b88be031841f565e66cf572e
alan galarza <alangalarza280@gmail.com>
2025-05-24 15:31:04 +0000

    add mru tk

---
+++ 02-respuestas/respuestas_data.py
@@ -0,0 +1,10 @@
+respuestas_correctas = 1
+respuestas_incorrectas = 2
+respuestas_en_blanco = 2
+
+respuestas_correctas_puntos = respuestas_correctas * 3
+respuestas_incorrectas_puntos = respuestas_incorrectas * -1
+respuestas_en_blanco_puntos = respuestas_en_blanco * 0
+
+
+print(f"respuestas del alumno: {r}")
---
+++ 02-respuestas/respuestas_input.py
@@ -0,0 +1,9 @@
+respuestas_correctas = input("ingrese las respuestas correctas: ")
+respuestas_incorrectas = input("ingrese las respuestas incorrectas: ")
+respuestas_en_blanco = input("ingrese las respuestas en blanco: ")
+
+respuesta_correcta_punto = float(respuestas_correctas) * 3
+respuesta_incorrecta_punto = float(respuestas_incorrectas) * -1
+respuesta_en_blanco_punto = float(respuestas_en_blanco) * 0
+r = float(respuestas_correctas) + float(respuestas_incorrectas) + float(respuestas_en_blanco)
+print(f"respuestas del alumno: {r}")
